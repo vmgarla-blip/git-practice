fbd6dabd1236ef2f4e09ec720da99a77868579e9
vmgarla-blip <vmgarla@gmail.com>
2026-05-22 03:26:46 -0500

    added special variable file

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,7 +2,7 @@
 NUMBER=$1
 if [ $NUMBER -gt 20 ]; then
     echo "Number is greater than 20"
-elif [ $NUMBER -eq 20]; then
+elif [ $NUMBER -eq 20 ]; then
     echo "Number is equal to 20"
 else
     echo "Number is less than 20"
