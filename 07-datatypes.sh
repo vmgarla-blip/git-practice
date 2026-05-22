4bd03935f3054e938253bb25ef8f57160fa11f02
vmgarla-blip <vmgarla@gmail.com>
2026-05-22 02:48:44 -0500

    added special variable file

--- 07-datatypes.sh
+++ 07-datatypes.sh
@@ -5,3 +5,8 @@ NUM2=30
 
 SUM=$(($NUM1+$NUM2))
 echo "Sum of $NUM1 and $NUM2 is $SUM"
+
+#Array
+MOVIES=("Inception" "Interstellar" "The Dark Knight")
+echo "First movie is ${MOVIES[0]}"
+echo "All movies are ${MOVIES[@]}"
