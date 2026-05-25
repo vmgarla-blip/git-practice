b0d7977975f45db01c938aef5f19553a84ec5e85
vmgarla-blip <vmgarla@gmail.com>
2026-05-25 00:54:03 -0500

    hello world script created

---
+++ 12-loops.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+USERID=$(id -u)
+#Check if user is root
+
+USERID=$(id -u)
+LOGS_DIR=/home/ec2-user/shell-logs
+LOGS_FILE=$LOGS_DIR/$0.log
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script as root user"
+    exit 1
+fi
+
+# first arg - what software you are trying to isntall
+# second arg - exit code
+VALIDATE () {
+
+    if [ $2 -ne 0  ]; then
+        echo "$1 installation failed"
+        exit 1
+    else
+        echo "$1 installed successfully"
+    fi
+
+}
+
+for package in $@
+do
+    echo "Installing $package..."
+
+
+done
+
