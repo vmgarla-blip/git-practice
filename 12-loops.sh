e87b9a1c9752b3d58546b0345fdbd5fe30097b72
vmgarla-blip <vmgarla@gmail.com>
2026-05-25 01:00:35 -0500

    hello world script created

--- 12-loops.sh
+++ 12-loops.sh
@@ -26,6 +26,13 @@ VALIDATE () {
 for package in $@
 do
     echo "Installing $package..."
+    dnf list installed $package 
+    if $? -ne 0  ; then
+        dnf install $package -y >> $LOGS_FILE
+        VALIDATE "Installing $package" $?
+    else
+        echo "$package already installed ... skipping installation"
+    fi
 
 
 done
