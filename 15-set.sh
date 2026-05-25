bb532acc3c027876a4c8531d03c637cf2de459e2
vmgarla-blip <vmgarla@gmail.com>
2026-05-25 02:08:28 -0500

    hello world script created

---
+++ 15-set.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+USERID=$(id -u)
+#Check if user is root
+
+USERID=$(id -u)
+LOGS_DIR=/home/ec2-user/shell-logs
+LOGS_FILE=$LOGS_DIR/$0.log
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script as root user"
+    exit 1
+fi
+
+# first arg - what software you are trying to isntall
+# second arg - exit code
+
+
+for package in $@
+do
+    echo "Installing $package..."
+    dnf list installed $package 
+    if [ $? -ne 0 ] ; then
+        dnf install $package -y >> $LOGS_FILE
+       s
+    else
+        echo -e "  $TIMESTAMP [INFO]  $package already installed ... $Y skipping $N" 
+    fi
+
+
+done
+
